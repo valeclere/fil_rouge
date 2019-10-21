843ed53b8c4bf4e60f58d2aa4e889a0eca6b68dc
Valentin LECLERE <Valentin.LECLERE@etu.uca.fr>
2019-10-21 14:40:49 +0200

    fin tp3

--- Liste.cpp
+++ Liste.cpp
@@ -6,19 +6,19 @@ Liste::Liste(): nb_r(0), nb_c(0)
 {}
 
 void Liste::Ajouter(Cercle c){
+    c.setOrdre(compteur);
+    cercles[nb_c]=c;
+
     compteur++;
     nb_c++;
-
-    c.setOrdre(compteur-1);
-    cercles[nb_c-1]=c;
 }
 
 void Liste::Ajouter(Rectangle r){
+    r.setOrdre(compteur);
+    rectangles[nb_r]=r;
+
     compteur++;
     nb_r++;
-
-    r.setOrdre(compteur-1);
-    rectangles[nb_r-1]=r;
 }
 
 int Liste::getNbC(){
@@ -34,14 +34,16 @@ std::string Liste::toString(){
     int courR=0;
     std::string Chaine="";
 
-    for (int i=0; i=compteur-1; i++)
-    {
-        if ((cercles[courC].getOrdre()==i) && (i<taille)){
+    for (int i=0; i<=compteur-1; i++)
+    {  
+        if (cercles[courC].getOrdre()==i)
+        {
             Chaine+=cercles[courC].toString()+" ";
             courC++;
         }
 
-        else{
+        else
+        {
             Chaine+=rectangles[courR].toString()+" ";
             courR++;
         }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int, char **)
 {
     Rectangle r(1,1,1,1);
     r.toString();
-    Cercle c1,c2, c3;
+    Cercle c1(2,2,2,2),c2(3,3,3,3), c3(4,4,5,5);
     std::cout << r.toString() << std::endl;
 
     Liste l0;
@@ -18,7 +18,7 @@ int main(int, char **)
 
     l0.Ajouter(c1); l0.Ajouter(r); l0.Ajouter(c2); l0.Ajouter(r); l0.Ajouter(c3);
 
-    std::cout << "LISTE : " << l0.toString() << std::endl;
+    std::cout << "LISTE  " << l0.toString() << std::endl;
 
 
     return 0;
